100cd7273b220550d0a17e4abdb28101ccf843b1
t0ri <victoriarosemurray@gmail.com>
2019-08-01 13:38:40 -0700

    set up components file structure

---
+++ src/Components/ShowMoods/ShowMoods.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import ShowMood from './ShowMood/ShowMood'
+
+export default class ShowMoods extends Component {
+  render() {
+    return (
+      <div>
+        ShowMoods
+        <ShowMood />
+      </div>
+    )
+  }
+}
