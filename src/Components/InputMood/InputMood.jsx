6a992d7148c0c0a5a6553f96e2c36256fdcf3382
t0ri <victoriarosemurray@gmail.com>
2019-08-01 13:43:50 -0700

    set up components file structure

---
+++ src/Components/InputMood/InputMood.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class InputMood extends Component {
+  render() {
+    return (
+      <div>
+        InputMood
+      </div>
+    )
+  }
+}
