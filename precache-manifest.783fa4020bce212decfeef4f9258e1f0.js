6b3e97e2f843ae01b90d5dbf2b07172e34181099
t0ri <victoriarosemurray@gmail.com>
2019-08-01 21:28:04 -0700

    Updates

---
+++ precache-manifest.783fa4020bce212decfeef4f9258e1f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e64774e6cd29445d480f97500fc93e",
+    "url": "/spd1.02-weather-app/index.html"
+  },
+  {
+    "revision": "a18b2e47c0445a53bd67",
+    "url": "/spd1.02-weather-app/static/css/main.e6e45bca.chunk.css"
+  },
+  {
+    "revision": "7685573bbbdc0ba85f9a",
+    "url": "/spd1.02-weather-app/static/js/2.2a3879e5.chunk.js"
+  },
+  {
+    "revision": "a18b2e47c0445a53bd67",
+    "url": "/spd1.02-weather-app/static/js/main.be31f40c.chunk.js"
+  },
+  {
+    "revision": "06038bb82bcb098f59fb",
+    "url": "/spd1.02-weather-app/static/js/runtime~main.82fb45e2.js"
+  }
+]);
